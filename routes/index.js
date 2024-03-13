60b93461850bdfe69afc481937e783f0f2bdbcd1
Serhii <slobojko21@gmail.com>
2024-03-13 18:30:51 +0200

    follow controller

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const {
   PostController,
   CommentController,
   LikeController,
+  FollowController,
 } = require("../controllers");
 const authenticateToken = require("../middleware/auth");
 
@@ -40,5 +41,12 @@ router.delete(
 // Like Routes
 router.post("/likes", authenticateToken, LikeController.likePost);
 router.delete("/likes/:id", authenticateToken, LikeController.unlikePost);
+// Follow Routes
+router.post("/follow", authenticateToken, FollowController.followUser);
+router.delete(
+  "/unfollow/:id",
+  authenticateToken,
+  FollowController.unfollowUser
+);
 
 module.exports = router;
